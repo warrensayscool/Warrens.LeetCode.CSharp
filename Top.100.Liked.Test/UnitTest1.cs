32db01c704c45e9f9fea937b993afc31450f0649
Warren Zheng <wzheng@sandfield.co.nz>
2024-07-02 14:32:02 +1200

    Add project files.

---
+++ Top.100.Liked.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Top._100.Liked.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Top.100.Liked/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Top._100.Liked
+{
+    public class Class1
+    {
+
+    }
+}
